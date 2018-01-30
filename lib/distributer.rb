2e1e587a409b8356f880970dacae4f74aa9c0e43
Jeffrey <jwan622@gmail.com>
2018-01-30 17:39:07 -0500

    finish tests

--- lib/distributer.rb
+++ lib/distributer.rb
@@ -1,8 +1,9 @@
 class Distributer
-  attr_reader :transactions
+  attr_reader :transactions, :client
 
   def initialize(transactions)
     @transactions = transactions
+    @client = JobcoinClient::Jobcoin.new
   end
 
   private
@@ -10,14 +11,14 @@ class Distributer
   def to_aggregate_account
     transactions.each do |trans|
       from_address = trans['toAddress']
-      remaining_balance = JobcoinClient::Jobcoin.new.address_transactions(from_address)['balance'].to_f
+      remaining_balance = client.address_transactions(from_address)['balance'].to_f
 
       # this will throw an error if we try to transfer an amount greater than
       # what's in the from_address or if we try to transfer 0.0. So, we will
       # only transfer an amount > 0
       # Also, if someone tries to transfer from house to an encrypted address, the transfer should
       # be returned back to the House Account here.
-      JobcoinClient::Jobcoin.new.add_transaction(
+      client.add_transaction(
         from_address,
         self.class::AGGREGATE_ACCOUNT,
         remaining_balance
@@ -30,7 +31,7 @@ class Distributer
 
     # make distributions from AGGREGATE_ACCOUNT to original accounts
     liabilities.each do |addr, amount|
-      JobcoinClient::Jobcoin.new.add_transaction(self.class::AGGREGATE_ACCOUNT, addr, amount)
+      client.add_transaction(self.class::AGGREGATE_ACCOUNT, addr, amount)
     end
   end
 end
